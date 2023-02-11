3a04471f2b33135c45691b8ee6108a8049495fb2
aqigif <aqilgifari0@gmail.com>
2023-02-12 02:34:49 +0700

    feat: create proxy for my-mapp frontend

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/maps/:place_id', (req, res) => {
+  const place_id = req.params.place_id;
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${apiKey}`;
+
+  axios.get(apiUrl)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('An error occurred while fetching data from the Google Maps API');
+    });
+});
+
+app.get('/maps/photo/:maxwidth/:photoreference', (req, res) => {
+  const photoreference = req.params.photoreference;
+  const maxwidth = req.params.maxwidth;
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoreference}&maxwidth=${maxwidth}&key=${apiKey}`;
+
+  axios.get(apiUrl, { responseType: 'arraybuffer' })
+    .then((response) => {
+      res.set('Content-Type', 'image/jpeg');
+      res.send(Buffer.from(response.data, 'binary'));
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('An error occurred while fetching data from the Google Maps API');
+    });
+});
+
+app.listen(port, () => {
+  console.log('Express proxy server for Google Maps API listening on port 3001');
+});
